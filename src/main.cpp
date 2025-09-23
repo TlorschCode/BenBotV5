7704e26a4ea762447288da9bcfdd08d7ca4934b7
Thomas <thomas.lorsch@gmail.com>
2025-09-22 20:31:16 -0700

    Full driver code

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,33 @@
 #include <string>
 #include <iostream>
 #include <thread>
+#include <cmath>
+#include <algorithm>
 
+// MARK: Hardware Init
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::Motor topLeft(1, pros::v5::MotorGearset::blue);
 pros::Motor bottomLeft(2, pros::v5::MotorGearset::blue);
 pros::Motor topRight(3, pros::v5::MotorGearset::blue);
 pros::Motor bottomRight(4, pros::v5::MotorGearset::blue);
 pros::Motor conveyor(5, pros::v5::MotorGearset::green);
-pros::Motor bandRotatorTop(6, pros::v5::MotorGearset::green);
+pros::Motor bandRotatorTop(6, pros::v5::MotorGearset::blue);
 pros::Motor bandRotatorBottom(7, pros::v5::MotorGearset::green);
+pros::Motor intake(8, pros::v5::MotorGearset::green);
 
 pros::Imu inertial(5);
 void configureMotors() {
 	topLeft.set_reversed(true);
 	bottomLeft.set_reversed(true);
-	conveyor.set_reversed(true);
+    conveyor.set_reversed(true);
+	intake.set_reversed(true);
 	topLeft.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
 	topRight.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
 	bottomLeft.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
 	bottomRight.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
 }
 
+// MARK: Variables
 const long double PI = 3.14159265358979323846;
 
 float posX = {0};
@@ -32,7 +38,25 @@ const float gear_ratio = {0.5f}; // Wheel-motor gear ratio
 const float wheel_circumference = {12.56f};
 const float frame = {100.0f / 1000.0f}; // Frame time
 float all_rot_prev = {0};
+enum Button {R1, L1, UP, DOWN, NONE};
+// enum ControllerButton {A, B, C, D, E,}
+float analog_left_x = {};
+float analog_left_y = {};
+float analog_right_x = {};
+float analog_right_y = {};
 
+// MARK: Templates
+void brakeWheels();
+void checkPauseProgram();
+void updateControllerData();
+void moveWheels(float speedLeft, float speedRight);
+
+
+
+
+
+
+// MARK: Utilities
 //| NON-DEFAULT FUNCTIONS |//
 inline double toRadians(float degrees) {
 	return degrees * (PI / 180);
@@ -82,20 +106,7 @@ void println(const T& input, int row = 1) {
     pros::lcd::set_text(row, printtext);
 }
 
-void moveWheels(float speedleft, float speedright) {
-	topLeft.move_velocity(speedleft);
-	bottomLeft.move_velocity(speedleft);
-	topRight.move_velocity(speedright);
-	bottomRight.move_velocity(speedright);
-}
-
-void brakeWheels() {
-	topLeft.brake();
-	bottomLeft.brake();
-	topRight.brake();
-	bottomRight.brake();
-}
-
+//| RUNTIME FUNCTIONS:
 void checkPauseProgram() { /// REMOVE THIS FUNCTION FOR FINAL COMPETITION
 	if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
 		brakeWheels();
@@ -103,6 +114,24 @@ void checkPauseProgram() { /// REMOVE THIS FUNCTION FOR FINAL COMPETITION
 			wait(10);
 		}
 	}
+}
+
+// MARK: Move Funcs
+//| MOVEMENT FUNCTIONS
+
+// Moves drivetrain wheels
+void moveWheels(float speedLeft, float speedRight) {
+	topLeft.move_velocity(speedLeft * 600);
+	bottomLeft.move_velocity(speedLeft * 600);
+	topRight.move_velocity(speedRight * 600);
+	bottomRight.move_velocity(speedRight * 600);
+}
+
+void brakeWheels() {
+	topLeft.brake();
+	bottomLeft.brake();
+	topRight.brake();
+	bottomRight.brake();
 }
 
 void trackPosition() {
@@ -158,48 +187,81 @@ void autonomous() {
 
 }
 
-void drivePipeline(float *driveSpeed) {
-	// Controller analog is -127 to 127
-	float analog_left_x = (pros::E_CONTROLLER_ANALOG_LEFT_X / 127) * 100;
-	float analog_left_y = (pros::E_CONTROLLER_ANALOG_LEFT_Y / 127) * 100;
-	float analog_right_x = (pros::E_CONTROLLER_ANALOG_RIGHT_X / 127) * 100;
-	float analog_right_y = (pros::E_CONTROLLER_ANALOG_RIGHT_Y / 127) * 100;
-	*driveSpeed += analog_right_y / 10;
-	float left = (analog_left_y * (*driveSpeed / 100)) + (analog_left_x * (*driveSpeed / 1.5));
-	float right = (analog_left_y * (*driveSpeed / 100)) - (analog_left_x * (*driveSpeed / 1.5));
-	moveWheels(left, right);
-	trackPosition();
+// MARK: Driving
+void drivePipeline(float driveSpeed) {
+	// Controller analog is -1 to 1
+	float left_speed = (analog_left_y * (driveSpeed / 100)) + (analog_left_x * (driveSpeed / 150));
+	float right_speed = (analog_left_y * (driveSpeed / 100)) - (analog_left_x * (driveSpeed / 150));
+	moveWheels(std::min(left_speed * 600, 600.0f), std::min(right_speed * 600, 600.0f));
+	// trackPosition();
 	println(posX);
 	println(posY, 2);
 }
 
-void scorePipeline() {
-	if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
-		conveyor.move_velocity(200);
-		bandRotatorTop.set_reversed(false);
-		bandRotatorTop.move_velocity(200);
-		bandRotatorBottom.move_velocity(200);
-	} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
-		conveyor.move_velocity(200);
-		bandRotatorTop.set_reversed(true);
-		bandRotatorTop.move_velocity(200);
-	} else {
-		conveyor.brake();
-		bandRotatorTop.brake();
+// MARK: Scoring
+void scorePipeline(float *driveSpeed, float *elevatorSpeed) {
+	float maxElevatorSpeed = 200.0f;
+	Button first_pressed = NONE;
+	if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP) and first_pressed != DOWN) {
+		first_pressed = UP;
+		*driveSpeed += 10;
+	} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) and first_pressed != UP) {
+		first_pressed = DOWN;
+		*driveSpeed -= 10;
 	}
 	if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-		
+		conveyor.move_velocity(*elevatorSpeed);
+		intake.brake();
+		bandRotatorBottom.brake();
+		bandRotatorTop.brake();
+		*elevatorSpeed -= 3;
+		*elevatorSpeed = std::max(*elevatorSpeed, -1.0f * maxElevatorSpeed);
+	} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) and first_pressed != L1) {
+		first_pressed = R1;
+		conveyor.move_velocity(*elevatorSpeed);
+		bandRotatorTop.set_reversed(true);
+		bandRotatorTop.move_velocity(275);
+		intake.move_velocity(200);
+		bandRotatorBottom.brake();
+		*elevatorSpeed += 3;
+		*elevatorSpeed = std::min(*elevatorSpeed, maxElevatorSpeed);
+	} else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) and first_pressed != R1) {
+		first_pressed = L1;
+		conveyor.move_velocity(*elevatorSpeed);
+		bandRotatorTop.set_reversed(false);
+		bandRotatorTop.move_velocity(275);
+		bandRotatorBottom.move_velocity(200);
+		intake.brake();
+		*elevatorSpeed += 3;
+		*elevatorSpeed = std::min(*elevatorSpeed, maxElevatorSpeed);
+	} else {
+		*elevatorSpeed = 0;
+		first_pressed = NONE;
+		conveyor.brake();
+		bandRotatorTop.brake();
+		intake.brake();
+		bandRotatorBottom.brake();
 	}
 }
 
+void updateControllerData() {
+	analog_left_x = (controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X) / 127);
+	analog_left_y = (controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) / 127);
+	analog_right_x = (controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / 127);
+	analog_right_y = (controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) / 127);
+}
+
+// MARK: opcontrol
 void opcontrol() {
 	// 72 inches across the field
 	float drive_speed = 100.0f;
+	float elevator_speed = 100.0f;
 	clear_screen();
 	wait(1000);
 	while (true) {
-		drivePipeline(&drive_speed);
-		scorePipeline();
+		updateControllerData();
+		drivePipeline(drive_speed);
+		scorePipeline(&drive_speed, &elevator_speed);
 		wait(frame);
 	}
 	controller.rumble("-");
